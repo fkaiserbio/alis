0e41b5d4a45b5be750b358bf8c9f70fe2733edca
Florian Kaiser <kaiser@pharm.ai>
2022-02-17 15:22:17 +0100

    fixed disk-by-id recognition - fixes an error for disk recognition

--- alis.sh
+++ alis.sh
@@ -479,8 +479,8 @@ function partition() {
       # load ZFS modules
       load_zfs
       # identify partition IDs
-      BOOT_PARTITION_ID="$(ls -n /dev/disk/by-id/ | grep $(basename $DEVICE)2 | awk '{print $(NF-2)}')"
-      ROOT_PARTITION_ID="$(ls -n /dev/disk/by-id/ | grep $(basename $DEVICE)3 | awk '{print $(NF-2)}')"
+      BOOT_PARTITION_ID="$(ls -n /dev/disk/by-id/ | grep $(basename $DEVICE)2 | head -1 | awk '{print $(NF-2)}')"
+      ROOT_PARTITION_ID="$(ls -n /dev/disk/by-id/ | grep $(basename $DEVICE)3 | head -1 | awk '{print $(NF-2)}')"
       echo $ROOT_PARTITION_ID
       create_zfs_datasets "$BOOT_PARTITION_ID" "$ROOT_PARTITION_ID"
     fi
